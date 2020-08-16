c36fbc6517f28383d2a04fe39fc4419f010a6b4b
YunJin Choi <zzdd1558@gmail.com>
2020-08-17 04:01:26 +0900

    preview Event

--- resources/javascripts/index.js
+++ resources/javascripts/index.js
@@ -4,31 +4,69 @@
     const previewClose = document.querySelector(".preview-close");
     const previewPrev = document.querySelector("#prev");
     const previewNext = document.querySelector("#next");
+    const previewImage = document.querySelector("#preview-image");
 
-    const closePreview = (e) => {
-        e.style.display = "none";
+    let index = 1;
+    const images = [
+        "resources/images/image-1.jpg",
+        "resources/images/image-2.jpg",
+        "resources/images/image-3.jpg",
+        "resources/images/image-9.jpg",
+        "resources/images/image-5.jpg",
+        "resources/images/image-6.jpg",
+        "resources/images/image-7.jpg",
+        "resources/images/image-8.jpg",
+        "resources/images/image-4.jpg"
+    ];
+
+    const showPreview = e => {
+        e.classList.remove("hidden")
     };
 
-    const positionIsHorizontal = v => v === 'horizontal';
-
-    const drawPreview = ({position , url}) => {
-        // 가로
-        if(positionIsHorizontal(position)) {
-
-            return;
-        }
+    const closePreview = e => {
+        e.classList.add("hidden");
     };
 
-    const previewEvent = (v) => {
+    const drawPreview = position => {
+        index = position;
+        previewImage.src = images[position];
+        showPreview(preview);
+    };
+
+    const previewEvent = v => {
         const {currentTarget : target} = v;
-
-        console.log(target.dataset.position);
-        console.log(target.src)
+        const {position} = target.dataset;
+        drawPreview(position)
     };
+
+    const isNext = (index , imageList) => {
+        const size = imageList.length - 1;
+        if ( index > size){
+            index = 0;
+        }
+
+        if( index < 0) {
+            index = size;
+        }
+        drawPreview(index);
+    }
+
+    const previewPrevEvent = () => {
+        index--;
+        isNext(index, images);
+    }
+    const previewNextEvent = () => {
+        index++;
+        isNext(index, images);
+    }
 
     previewItem.forEach(v => {
         v.addEventListener("click" , previewEvent);
     });
 
     previewClose.addEventListener("click" , () => {closePreview(preview)})
+
+    previewPrev.addEventListener("click", previewPrevEvent);
+    previewNext.addEventListener("click", previewNextEvent);
+
 })();
