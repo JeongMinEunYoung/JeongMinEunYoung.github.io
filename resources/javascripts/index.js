9b2b2d93f10ab0460ac82ec93cc685f7fcdb36b5
YunJin Choi <zzdd1558@gmail.com>
2020-08-17 04:46:30 +0900

    zoom

--- resources/javascripts/index.js
+++ resources/javascripts/index.js
@@ -70,8 +70,11 @@
     previewNext.addEventListener("click", previewNextEvent);
 
 
-    // 마커가 표시될 위치입니다
+    // 마커
     var markerPosition  = new kakao.maps.LatLng(37.2624598,127.0241684);
+
+    // 지도 확대 축소
+    var zoomControl = new kakao.maps.ZoomControl();
 
     var mapContainer = document.getElementById('map'), // 지도를 표시할 div
         mapOption = {
@@ -80,13 +83,15 @@
         };
 
     var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+    map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
 
     // 마커를 생성합니다
     var marker = new kakao.maps.Marker({
         position: markerPosition
     });
 
+
+
     // 마커가 지도 위에 표시되도록 설정합니다
     marker.setMap(map);
-
 })();
