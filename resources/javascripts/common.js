7fe3d227afbccee2524c255ac450fea054f42f73
YunJin Choi <zzdd1558@gmail.com>
2020-08-25 22:33:48 +0900

    test

--- resources/javascripts/common.js
+++ resources/javascripts/common.js
@@ -11,14 +11,5 @@
 
     g(d, 100), g(i, 250), g(m, 350), g(u, 350), g(l, 500), g(p, 550);
 
-    const preload = new Image();
-    const v = [
-        "resources/images/JUN_Main.jpg","resources/compressed/JUN_1527.jpg", "resources/images/call_icon.png","resources/images/message_icon.png",
-            "resources/compressed/image-1.jpg", "resources/compressed/image-2.jpg",
-        "resources/compressed/image-3.jpg", "resources/compressed/image-9.jpg", "resources/compressed/image-5.jpg", "resources/compressed/image-6.jpg",
-        "resources/compressed/image-7.jpg", "resources/compressed/image-8.jpg",
-            "resources/compressed/image-4.jpg"];
-
-    v.forEach(v => preload.src = v);
 
 })();
---
+++ resources/javascripts/load.js
@@ -0,0 +1,10 @@
+
+const preload = new Image();
+const v = [
+    "resources/images/JUN_Main.jpg","resources/compressed/JUN_1527.jpg", "resources/images/call_icon.png","resources/images/message_icon.png",
+    "resources/compressed/image-1.jpg", "resources/compressed/image-2.jpg",
+    "resources/compressed/image-3.jpg", "resources/compressed/image-9.jpg", "resources/compressed/image-5.jpg", "resources/compressed/image-6.jpg",
+    "resources/compressed/image-7.jpg", "resources/compressed/image-8.jpg",
+    "resources/compressed/image-4.jpg"];
+
+v.forEach(v => preload.src = v);
